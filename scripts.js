776d3a1b5525ad9e53c1aa2e1a4c6a86ca71b2a2
Zuzana Davidová <88673168+Zuzik761@users.noreply.github.com>
2021-08-22 13:45:20 +0200

    uvod

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log("JavaScript is working");
+
+let colors = ["orange", "blue", "red"]
+let post = {
+    tile: "my Post",
+    description: "My Fisrt post",
+    likeCount: 12,
+    comments: ["cool!", "nice job!"]
+}
+console.log(colors)
+console.log(post)
+
+let age = 33;
+
+if (age>18) {console.log ("YOU ARE AN ADULT!");
+}
