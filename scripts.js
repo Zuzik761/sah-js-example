5bd84d520ce833f19c15ac7a897fcdb66a2c6561
Zuzana Davidová <88673168+Zuzik761@users.noreply.github.com>
2021-08-22 15:02:48 +0200

    uprava scriptu

--- scripts.js
+++ scripts.js
@@ -1,16 +1,50 @@
 console.log("JavaScript is working");
 
-let colors = ["orange", "blue", "red"]
+let colors = ["orange", "blue", "red", "white", "black"]
 let post = {
     tile: "my Post",
     description: "My Fisrt post",
     likeCount: 12,
     comments: ["cool!", "nice job!"]
 }
+
+let posts = [{
+title: "First Post",
+likeCount: 12,
+}, {
+    title: "Second Post",
+    likeCount: 222
+}
+]
+
 console.log(colors)
 console.log(post)
 
-let age = 33;
+let age = 18;
 
-if (age>18) {console.log ("YOU ARE AN ADULT!");
+if (age>=18) {console.log ("YOU ARE AN ADULT!");
+} else
+{console.log ("your are a kid !")
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log (i);
+    }
+    
+    console.log (colors);
+
+let greeter = name => {
+        console.log (`Hello, ${name}`);
+    }
+        
+   greeter("Adam");
+   greeter("Eva");
+   greeter("Josef");
+   greeter("Maria");
+     
+
+    colors.forEach(color => {
+        console.log(color)
+    });
+
+    console.log("This is the end of our code");
